17ca1f4619a093b288353ed51ac5a2a1ae028aea
BeeMao03 <153680716+BeeMao03@users.noreply.github.com>
2024-03-04 18:36:19 -0300

    Install bootstrap and add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>Code built by Mariel Buzarquis</footer>
+    </div>
+  );
 }
