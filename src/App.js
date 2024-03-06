a243abe035f71b7e37afc306bc0a13fa95c24459
BeeMao03 <153680716+BeeMao03@users.noreply.github.com>
2024-03-05 21:21:42 -0300

    Add HTML and CSS structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1> Weather App</h1>
-        <footer>Code built by Mariel Buzarquis</footer>
+    <body>
+      <div className="App">
+        <div className="container">
+          <Weather />
+          <footer>
+            Open-source code built by{" "}
+            <a href="https://github.com/BeeMao03" target="_blank">
+              Mariel Buzarquis
+            </a>
+            , available on{" "}
+            <a
+              href="https://github.com/BeeMao03/react-weather-app-mao"
+              target="_blank">
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://weather-app-react-mao.netlify.app/"
+              target="_blank">
+              Netlify
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-search w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Asuncion</h1>
+      <ul>
+        <li>Tuesday 18:14</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+              className="icon"
+            />
+            <span className="temperature">30</span>{" "}
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 60%</li>
+            <li>Wind: 2km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
